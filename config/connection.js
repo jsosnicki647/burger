e62b3e62df161e84cbae1175a3373fb033a9cc9d
Jesse Sosnicki <jesse.sosnicki@gmail.com>
2019-09-26 21:37:42 -0400

    fixing port

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
 const mysql = require("mysql")
 let connection
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection({port: process.env.JAWSDB_URL})
-// }
-// else{
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else{
   connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
+    port: 3306 || process.env.PORT,
     user: "root",
     password: "Jeannie63",
     database: "burgers_db"
   })
-// }
+}
 
 connection.connect((err) => {
   if (err) {
